433ae34633f1cbeafdb6d2f161347f9639036a0b
Andrei Shamrei <Andrei.Shamrei@coxautoinc.com>
2023-07-17 23:33:40 +0300

    Task 9 - Docker and AWS Elastic Beanstalk

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
 const API_PATHS = {
   product: "https://lrznfwuq7h.execute-api.eu-north-1.amazonaws.com",
-  order: "https://sqebw0c7m2.execute-api.eu-north-1.amazonaws.com/api/profile",
+  order:
+    "http://andrewshamrey-cart-api-prod.eu-north-1.elasticbeanstalk.com/api/profile",
   import: "https://n3pn2plh42.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://lrznfwuq7h.execute-api.eu-north-1.amazonaws.com",
-  cart: "https://sqebw0c7m2.execute-api.eu-north-1.amazonaws.com/api",
+  cart: "http://andrewshamrey-cart-api-prod.eu-north-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
